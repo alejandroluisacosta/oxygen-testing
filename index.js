e3b0f03743f1d0130038b32d8b294c929748f3b5
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-01 17:13:02 +0200

    add: occupiedPercentage tests

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 class Room {
-    constructor() {
-
+    constructor(name, bookings, rate, discount) {
+        this.name = name;
+        this.bookings = bookings || [];
+        this.rate = rate || 0;
+        this.discount = discount || 0;
     }
 
     isOccupied(date) {
-
+        const checkDate = new Date(date);
+        if (this.bookings) {
+            return this.bookings.some(booking => {
+                const checkIn = new Date(booking.checkIn);
+                const checkOut = new Date(booking.checkOut);
+                return checkDate >= checkIn && checkDate < checkOut;
+            });
+        }
+        return false;
     }
 
     occupancyPercentage(start, end) {
-
+        const startDate = new Date(start);
+        const endDate = new Date(end);
+        if (this.bookings) {
+            
+        }
     }
 
     static totalOccupancyPercentage(rooms, startDate, endDate) {
--- index.test.js
+++ index.test.js
@@ -1,23 +1,114 @@
 const { Room } = require('./index');
 
-const roomTemplate = new Room({name: 'Triple Pleasure', rate: 10000, discount: 25});
+const roomTemplate = new Room({name: 'Suite', rate: 10000, discount: 25});
+const bookingsTemplate = [
+    {
+        checkIn: '2024-01-01',
+        checkOut: '2024-01-04'
+    },
+    {
+        checkIn: '2024-02-01',
+        checkOut: '2024-02-04'
+    },
+    {
+        checkIn: '2024-03-30',
+        checkOut: '2024-04-05'
+    },
+];
 
-test('Checks if room is occupied on a given date', () => {
-    const room = {...roomTemplate};
-    const bookings = [
-        {
-            checkIn: '2024-01-01',
-            checkOut: '2024-01-04'
-        },
-        {
-            checkIn: '2024-02-01',
-            checkOut: '2024-02-04'
-        },
-        {
-            checkIn: '2024-03-01',
-            checkOut: '2024-03-07'
-        },
-    ]
-    room.bookings = bookings;
+// isOccupied
+
+test('Check if room is occupied on date that is not the last day of any booking', () => {
+    const room = new Room({...roomTemplate});
+    room.bookings = bookingsTemplate;
+
+    expect(room.isOccupied('2024-01-01')).toBe(true);
+    expect(room.isOccupied('2024-01-06')).toBe(false);
+    expect(room.isOccupied('2024-02-02')).toBe(true);
+    expect(room.isOccupied('2024-02-03')).toBe(true);
+    expect(room.isOccupied('2024-03-01')).toBe(false);
+    expect(room.isOccupied('2024-03-30')).toBe(true);
+    expect(room.isOccupied('2024-04-03')).toBe(true);
+
+});
+
+test('Check if room is occupied on last day of a booking', () => {
+    const room = new Room({...roomTemplate});
+    room.bookings = bookingsTemplate;
+
+    expect(room.isOccupied('2024-01-04')).toBe(false);
+    expect(room.isOccupied('2024-02-04')).toBe(false);
+    expect(room.isOccupied('2024-04-05')).toBe(false);
+});
+
+test('Check if room is occupied if bookings array has falsy value (empty/undefined/null)', () => {
+    const room1 = new Room({...roomTemplate});
+    room1.bookings = [];
+    
+    const room2 = new Room({...roomTemplate});
+    room2.bookings = undefined;
+    
+    const room3 = new Room({...roomTemplate});
+    room3.bookings = null;
+
+    expect(room1.isOccupied('2024-01-01')).toBe(false);
+    expect(room2.isOccupied('2024-01-01')).toBe(false);
+    expect(room3.isOccupied('2024-01-01')).toBe(false);
+});
+
+test('Check if room is occupied if date is equal to last day of a booking', () => {
+    const room = new Room({...roomTemplate});
+    room.bookings = bookingsTemplate;
+
+    expect(room.isOccupied('2024-01-04')).toBe(false);
+    expect(room.isOccupied('2024-02-04')).toBe(false);
+    expect(room.isOccupied('2024-04-05')).toBe(false);
+});
+
+// occupancyPercentage
+
+test('Confirm occupancy is 100% if room is occupied every day in the provided range', () => {
+    const room = new Room({...roomTemplate});
+    room.bookings = bookingsTemplate;
+
+    expect(room.occupancyPercentage('2024-01-01', '2024-01-04')).toBe(100);
+    expect(room.occupancyPercentage('2024-02-01', '2024-02-04')).toBe(100);
+    expect(room.occupancyPercentage('2024-03-30', '2024-04-05')).toBe(100);
+});
+
+test('Confirm occupancy is 0% if room is not occupied any day in the provided range', () => {
+    const room = new Room({...roomTemplate});
+    room.bookings = bookingsTemplate;
+
+    expect(room.occupancyPercentage('2024-01-05', '2024-01-31')).toBe(0);
+    expect(room.occupancyPercentage('2024-02-04', '2024-03-29')).toBe(0);
+    expect(room.occupancyPercentage('2024-04-06', '2024-04-06')).toBe(0);
+});
+
+test('Check if room is occupied some days in the provided range', () => {
+    const room = new Room({...roomTemplate});
+    room.bookings = bookingsTemplate;
+    room.bookings.push({checkIn: '2024-04-09', checkOut: '2024-04-30'});
+
+    expect(room.occupancyPercentage('2024-01-02', '2024-01-03')).toBe(66.66);
+    expect(room.occupancyPercentage('2024-03-31', '2024-04-04')).toBe(71.43);
+    expect(room.occupancyPercentage('2024-04-09', '2024-04-19')).toBe(50);
 })
 
+test('Confirm occupancy is 0% if no booking are provided for the room', () => {
+    const room = new Room({...roomTemplate});
+    room.bookings = [];
+
+    expect(room.occupancyPercentage('2024-01-01', '2024-01-04')).toBe(0);
+    expect(room.occupancyPercentage('2024-01-02', '2024-01-03')).toBe(0);
+    expect(room.occupancyPercentage('2024-03-31', '2024-04-04')).toBe(0);
+});
+
+test('Check occupancy if check-in and check-out dates are equal', () => {
+    const room = new Room({...roomTemplate});
+    room.bookings = bookingsTemplate;
+
+    expect(room.occupancyPercentage('2024-01-01', '2024-01-01')).toBe(100);
+    expect(room.occupancyPercentage('2024-01-15', '2024-01-15')).toBe(0);
+    expect(room.occupancyPercentage('2024-03-30', '2024-03-30')).toBe(100);
+});
